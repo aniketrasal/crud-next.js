b3fc6940e6e556e5bc1bfb01b267d4463249e6ce
Aniket Rasal <rasalaniket00@gmail.com>
2024-02-07 00:18:40 +0530

    input field updated

--- src/app/Components/FirebaseCrud/Navbar.js
+++ src/app/Components/FirebaseCrud/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
 
 <nav className="bg-white border border-blue w-full">
     <div className="flex flex-wrap items-left p-5 border border-gray-200 w-full">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://thepric.com/wp-content/uploads/2021/08/play_store_512-removebg-preview-1.png" className="h-10" alt="Flowbite Logo" />
             <span className="text-3xl font-semibold whitespace-nowrap dark:text-white">Pric</span>
         </a>
